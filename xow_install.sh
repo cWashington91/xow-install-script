81b63d879926d8aede72bac3d95f02eca3a4a987
cWashington91 <christavo.washington@protonmail.com>
2021-05-15 15:46:50 -0500

    Add more to the script

--- xow_install.sh
+++ xow_install.sh
@@ -2,15 +2,75 @@
 
 XOW_REPO = https://github.com/medusalix/xow
 
-check_dependencies () {
+if grep -qs "ubuntu" /etc/os-release; then
+    os="ubuntu"
+elif [[ -e /etc/os_release ]]; then
+	os="pop"
+elif [[ -e /etc/debian_version ]]; then
+	os="debian"
+else [[ -e /etc/fedora-release ]]; then
+	os="fedora"
+fi
 
+check_kernel () {
+    if [[ $(uname -r | cut -d "." -f 1) -lt 4 ]]; then
+        echo "Kernel 4.5 or higher is needed to install xow"
+    else
+        continue
+    fi
 }
 
-clone_xow_repo () {
+check_systemd () {
+    
+}
+
+check_curl () {
+    if [[ $os == "ubuntu" || $os == "pop" || $os == "debian" ]]; then
+        dpkg --list | grep -s curl
+        if [[ -e curl ]]; then
+            continue
+        else
+            echo "curl is not installed. Installing..."
+            sudo apt update && sudo apt upgrade
+            sudo apt install -y curl
+    fi
+}
+
+check_cabextract () {
+    if [[ $os == "ubuntu" || $os == "pop" || $os == "debian" ]]; then
+        dpkg --list | grep -s cabextract
+        if [[ -e cabextract ]]; then
+            continue
+        else
+            echo "cabextract is not installed. Installing..."
+            sudo apt update && sudo apt upgrade
+            sudo apt install -y cabextract
+    fi
+}
+
+check_libusb () {
+    if [[ $os == "ubuntu" || $os == "pop" || $os == "debian" ]]; then
+        dpkg --list | grep -s libusb
+        if [[ -e libusb ]]; then
+            continue
+        else
+            echo "libusb is not installed. Installing..."
+            sudo apt update && sudo apt upgrade
+            sudo apt install -y libusb-1.0-0-dev
+    fi
+}
+
+
+clone_xow () {
     git clone $XOW_REPO
 }
 
 build_xow () {
+    cd xow
+    make BUILD=RELEASE
+}
+
+install_xow () {
     sudo make install
     sudo systemctl enable xow
     sudo systemctl start xow
@@ -18,12 +78,11 @@ build_xow () {
 
 optional_reboot () {    
     read -p "Install complete, would you like to reboot now? y|n" reboot
-    if [ $reboot == y ]; then
-      shutdown -r -t 3
-      echo "rebooting"
-    elif [ $reboot == n ]; then
-      break
+    if [[ $reboot == y ]]; then
+        shutdown -r now
+    elif [[ $reboot == n ]]; then
+        break
     else
-      echo "Please provide a valid response"
+        echo "Please provide a valid response"
     fi
 }
