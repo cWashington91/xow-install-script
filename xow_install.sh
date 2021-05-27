cd938dd951c368e7b95a33ab50e714f3ca80e989
cWashington91 <christavo.washington@protonmail.com>
2021-05-27 18:02:30 -0500

    Add Solus support

--- xow_install.sh
+++ xow_install.sh
@@ -6,6 +6,8 @@ if grep -qs "ubuntu" /etc/os-release; then
     os="ubuntu"
 elif [[ -e /etc/os_release ]]; then
 	os="pop"
+elif [[ -e /etc/os-release ]]; then
+	os="solus"
 elif [[ -e /etc/debian_version ]]; then
 	os="debian"
 else [[ -e /etc/fedora-release ]];
@@ -15,6 +17,8 @@ fi
 install_prereqs () {
     if [[ $os == "ubuntu" || $os == "pop" || $os == "debian" ]]; then
         sudo apt update && sudo apt -y install build-essential curl cabextract libusb-1.0-0-dev
+    elif [[ $os == "solus" ]]; then
+        sudo eopkg install -y -c system.devel curl cabextract libusb-compat-devel
     else [[ $os == "fedora" ]];
         sudo dnf install -y make automake gcc gcc-c++ kernel-devel curl cabextract libusb-devel
     fi
@@ -36,10 +40,10 @@ install_xow () {
 }
 
 optional_reboot () {    
-    read -p "Install complete, would you like to reboot now? y|n" reboot
-    if [[ $reboot == y ]]; then
+    read -p "Install complete, would you like to reboot now? (yes|no)" reboot
+    if [[ $reboot == yes ]]; then
         shutdown -r now
-    elif [[ $reboot == n ]]; then
+    elif [[ $reboot == no ]]; then
         exit 0
     else
         echo "Please provide a valid response"
@@ -51,3 +55,4 @@ clone_xow
 build_xow
 install_xow
 optional_reboot
+
